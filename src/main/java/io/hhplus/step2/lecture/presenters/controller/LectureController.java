b57b98d8ab09be964111b877c8a49f11f628d559
Wonjuyeon <dnjswndus95@naver.com>
2024-06-25 23:18:34 +0900

    [:sparkles:] - 컨트롤러 강의목록 조회 api 추가

--- src/main/java/io/hhplus/step2/lecture/presenters/controller/LectureController.java
+++ src/main/java/io/hhplus/step2/lecture/presenters/controller/LectureController.java
@@ -1,30 +1,58 @@
 package io.hhplus.step2.lecture.presenters.controller;
 
+import io.hhplus.step2.lecture.application.service.LectureService;
+import io.hhplus.step2.lecture.dto.LectureListElement;
 import io.hhplus.step2.lecture.dto.request.ApplyLecturesRequest;
+import io.hhplus.step2.lecture.dto.request.GetUserLectureApplicationWhetherRequest;
 import io.hhplus.step2.lecture.dto.response.ApplyLecturesResponse;
+import io.hhplus.step2.lecture.dto.response.GetLectureListResponse;
 import io.hhplus.step2.lecture.dto.response.GetUserLectureApplicationWhetherResponse;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/lectures")
 @RequiredArgsConstructor
 public class LectureController {
 
-    @GetMapping("/application/{userId}")
+    private final LectureService lectureService;
+
+    @PostMapping("/application")
     public GetUserLectureApplicationWhetherResponse GetUserLectureApplicationWhether(
-            @PathVariable Long userId
-    ) {
-        return GetUserLectureApplicationWhetherResponse.builder()
+            @RequestBody GetUserLectureApplicationWhetherRequest request
+            ) {
+        return this.lectureService.getUserLectureApplicationWhether(request);
+
+        /*return GetUserLectureApplicationWhetherResponse.builder()
                 .isApplicationLecture(true)
-                .build();
+                .build();*/
     }
 
     @PostMapping("/apply")
     public ApplyLecturesResponse ApplyLectures(@RequestBody ApplyLecturesRequest request) {
 
-        return ApplyLecturesResponse.builder()
+        return this.lectureService.applyLectures(request);
+
+        /*return ApplyLecturesResponse.builder()
                 .isSuccess(true)
+                .build();*/
+    }
+
+    @GetMapping("")
+    public GetLectureListResponse getLectureList() {
+        LectureListElement element1 = new LectureListElement(1L, "강의 이름1", 29, 30);
+        LectureListElement element2 = new LectureListElement(2L, "강의 이름2", 28, 30);
+
+        List<LectureListElement> lectures = new ArrayList<>();
+        lectures.add(element1);
+        lectures.add(element2);
+
+        return GetLectureListResponse.builder()
+                .lectures(lectures)
                 .build();
     }
 
--- src/test/java/io/hhplus/step2/lecture/LectureControllerTest.java
+++ src/test/java/io/hhplus/step2/lecture/LectureControllerTest.java
@@ -2,8 +2,11 @@ package io.hhplus.step2.lecture;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.hhplus.step2.lecture.dto.LectureListElement;
 import io.hhplus.step2.lecture.dto.request.ApplyLecturesRequest;
+import io.hhplus.step2.lecture.dto.request.GetUserLectureApplicationWhetherRequest;
 import io.hhplus.step2.lecture.dto.response.ApplyLecturesResponse;
+import io.hhplus.step2.lecture.dto.response.GetLectureListResponse;
 import io.hhplus.step2.lecture.dto.response.GetUserLectureApplicationWhetherResponse;
 import io.hhplus.step2.lecture.presenters.controller.LectureController;
 import org.junit.jupiter.api.Test;
@@ -13,6 +16,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @WebMvcTest(LectureController.class)
 public class LectureControllerTest {
@@ -31,11 +39,19 @@ public class LectureControllerTest {
                 .isApplicationLecture(true)
                 .build();
 
+        GetUserLectureApplicationWhetherRequest request = GetUserLectureApplicationWhetherRequest.builder()
+                .userId(1L)
+                .lectureId(1L)
+                .build();
+
+        String requestJson = this.objectMapper.writeValueAsString(request);
+
         // when
         // then
-        this.mockMvc.perform(MockMvcRequestBuilders.get("/lectures/application/{userId}", userId)
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.isApplicationLecture").value(expectedResponse.isApplicationLecture()))
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/lectures/application")
+                .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(jsonPath("$.isApplicationLecture").value(expectedResponse.isApplicationLecture()))
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
@@ -59,6 +75,35 @@ public class LectureControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                         .content(requestJson))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.isSuccess").value(expectedResponse.isSuccess()));
+                .andExpect(jsonPath("$.isSuccess").value(expectedResponse.isSuccess()));
+    }
+
+    @Test
+    void 특강리스트조회() throws Exception {
+        // given
+        LectureListElement element1 = new LectureListElement(1L, "강의 이름1", 29, 30);
+        LectureListElement element2 = new LectureListElement(2L, "강의 이름2", 28, 30);
+
+        List<LectureListElement> lectures = new ArrayList<>();
+        lectures.add(element1);
+        lectures.add(element2);
+
+        GetLectureListResponse expectedResponse = GetLectureListResponse.builder()
+                .lectures(lectures)
+                .build();
+
+        // when
+        // then
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/lectures"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$.lectures").isArray())
+                .andExpect(jsonPath("$.lectures[0].id").value(1))
+                .andExpect(jsonPath("$.lectures[0].lectureName").value("강의 이름1"))
+                .andExpect(jsonPath("$.lectures[0].currentPersonCount").value(29))
+                .andExpect(jsonPath("$.lectures[0].limitPersonCount").value(30))
+                .andExpect(jsonPath("$.lectures[1].id").value(2))
+                .andExpect(jsonPath("$.lectures[1].lectureName").value("강의 이름2"))
+                .andExpect(jsonPath("$.lectures[1].currentPersonCount").value(28))
+                .andExpect(jsonPath("$.lectures[1].limitPersonCount").value(30));
     }
 }
